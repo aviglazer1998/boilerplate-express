e7793f33c0df145d95f071784aabeeadbbd7c4e6
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-11-07 13:24:02 -0500

    completed challanges

--- myApp.js
+++ myApp.js
@@ -4,11 +4,11 @@ var app = express();
 console.log("Hello World");
 
 app.get("/", function (req, res) {
-	res.send("Hello Express");
-});
-
-app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
