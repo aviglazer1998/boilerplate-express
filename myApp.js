5e14545e9dd38b41fcbbc30cb5ad2eaf90f00efa
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-11-09 10:18:47 -0500

    Done

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var express = require("express");
+const bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var app = express();
-require("dotenv").config();
 
 console.log("Hello World");
 
@@ -8,21 +10,68 @@ app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use(express.static(__dirname + "/public"));
-
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => {
-	var response = "Hello json".toUpperCase();
+app.use(function (req, res, next) {
+	console.log(req.method + " " + req.path + " - " + req.ip);
+	next();
+});
 
-	if (process.env.VAR_NAME === "uppercase") {
-		response = "Hello josn".toUpperCase();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/json", (req, res) => {
+	const mySecret = process.env["MESSAGE_STYLE"];
+	if (mySecret === "uppercase") {
+		res.json({
+			message: "HELLO JSON",
+		});
 	} else {
-		response = "Hello json";
+		res.json({
+			message: "Hello json",
+		});
 	}
+});
+
+app.use(function middleware(req, res, next) {
+	var string = req.method + " " + req.path + " - " + req.ip;
+	console.log(string);
+	next();
+});
+
+app.get(
+	"/now",
+	(req, res, next) => {
+		req.time = new Date().toString();
+		next();
+	},
+	(req, res) => {
+		res.send({
+			time: req.time,
+		});
+	}
+);
+
+app.get("/:word/echo", (req, res) => {
+	const { word } = req.params;
 	res.json({
-		message: response,
+		echo: word,
 	});
 });
+
+app.get("/name", (req, res) => {
+	var firstName = req.query.first;
+	var lastName = req.query.last;
+
+	res.json({
+		name: `${firstName} ${lastName}`,
+	});
+});
+
+app.post("/name", function (req, res) {
+	var string = req.body.first + " " + req.body.last;
+	res.json({ name: string });
+});
+
 
 module.exports = app;
